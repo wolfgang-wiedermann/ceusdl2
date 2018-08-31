cdfbf92cf4f5d77370916684d4b714992224140a
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2018-08-31 14:17:47 +0200

    Erkennung von Duplikaten bei der Attributdefinition innerhalb der Interfaces eingebaut

--- CeusDL.Parser/Validator/CoreInterfaceValidator.cs
+++ CeusDL.Parser/Validator/CoreInterfaceValidator.cs
@@ -19,6 +19,43 @@ namespace KDV.CeusDL.Validator {
             if(DimHasNonSingleAttributePrimaryKey(ifa)) {
                 repo.AddError($"Your ceusdl code for {ifa.Name} must have a one attribute primary key", ValidationResult.OT_INTERFACE);
             }
+
+            var duplicateAttributes = FindDuplicateAttributes(ifa, m);
+            foreach(var duplicate in duplicateAttributes) {
+                repo.AddError($"Your code for {ifa.Name} contains more then one definition of {duplicate} as illegal duplicates", ValidationResult.OT_ATTRIBUTE);
+            }
+        }
+
+        private static List<string> FindDuplicateAttributes(CoreInterface ifa, CoreModel m)
+        {
+            var result = new HashSet<string>();
+
+            foreach(var attr in ifa.Attributes) {
+                if(attr is CoreBaseAttribute || attr is CoreFactAttribute) {
+                    var matches = ifa.Attributes
+                                     .Where(a => a is CoreBaseAttribute || a is CoreFactAttribute)
+                                     .Select(a => (CoreBaseAttribute)a)
+                                     .Where(a => a.Name == attr.Name);
+
+                    if(matches.Count() > 1) {
+                        result.Add(attr.Name);
+                    }
+                } else if(attr is CoreRefAttribute) {
+                    var refAttr = (CoreRefAttribute)attr;
+                    var matches = ifa.Attributes
+                                     .Where(a => a is CoreRefAttribute)
+                                     .Select(a => (CoreRefAttribute)a)
+                                     .Where(a => a.Alias == refAttr.Alias 
+                                        && a.ReferencedInterface.Name == refAttr.ReferencedInterface.Name
+                                        && a.ReferencedAttribute.Name == refAttr.ReferencedAttribute.Name);
+
+                    if(matches.Count() > 1) {
+                        result.Add(attr.Name);
+                    }
+                }
+            }
+
+            return result.ToList();
         }
 
         private static bool DimHasNonSingleAttributePrimaryKey(CoreInterface ifa)
