4ce7e336c55dfe16a01d518e2423b5fd93b06b98
Wolfgang Wiedermann <wolfgang.wiedermann@oth-regensburg.de>
2024-01-17 09:17:33 +0100

    Bug 4991: Aktualisierung System.Data.SqlClient wegen CVE-2024-0056

--- CeusDL/Program.cs
+++ CeusDL/Program.cs
@@ -19,11 +19,11 @@ namespace CeusDL2
 {
     ///
     /// Eine Windows-Exe kompilieren mit Aufruf 
-    /// dotnet publish -c Release -r win7-x64 --self-contained 
+    /// dotnet publish -c Release -r win-x64 --self-contained -p:PublishTrimmed=true
     /// Native ausführbare Datei für MacOS kompilieren
-    /// dotnet publish -c Release -r osx.10.11-x64
+    /// dotnet publish -c Release -r osx-x64 --self-contained -p:PublishTrimmed=true
     /// Native ausführbare Datei für Linux kompilieren
-    /// dotnet publish -c Release -r linux-x64
+    /// dotnet publish -c Release -r linux-x64 --self-contained -p:PublishTrimmed=true
     /// in der Commandline
     ///
     class Program
